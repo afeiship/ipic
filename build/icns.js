050e2f498ecd36126f4ac330eb1dbdaf400ed902
afeiship <1290657123@qq.com>
2019-07-08 21:39:52 +0800

    add: gulp build script

--- build/icns.js
+++ build/icns.js
@@ -1,7 +1,19 @@
-const fs = require('fs');
-const icnsConvert = require('@fiahfy/icns-convert');
+(function() {
+  ('use strict');
 
-const buf = fs.readFileSync('./src/assets/wln_active.png'); // squre, 1024x1024 pixels or more
-icnsConvert(buf).then((data) => {
-  fs.writeFileSync('output.icns', data);
-});
+  const fs = require('fs');
+  const icnsConvert = require('@fiahfy/icns-convert');
+
+  var gulp = require('gulp');
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
+  });
+
+  //clean
+  gulp.task('icns', function() {
+    const buf = fs.readFileSync('./src/assets/wln_active.png'); // squre, 1024x1024 pixels or more
+    return icnsConvert(buf).then((data) => {
+      fs.writeFileSync('output.icns', data);
+    });
+  });
+})();
--- build/osx.js
+++ build/osx.js
@@ -1,13 +1,25 @@
-const packager = require('electron-packager');
+(function() {
+  ('use strict');
 
-// https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#extend-info
-packager({
-  dir: '.',
-  platform: 'darwin',
-  out: 'dist',
-  icon: './src/assets/bird.icns',
-  overwrite: true,
-  extendInfo: {
-    LSUIElement: '1'
-  }
-});
+  const gulp = require('gulp');
+  const packager = require('electron-packager');
+  const $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
+  });
+
+  // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#extend-info
+
+  //clean
+  gulp.task('osx:pack', function() {
+    return packager({
+      dir: '.',
+      platform: 'darwin',
+      out: 'dist',
+      icon: './src/assets/bird.icns',
+      overwrite: true,
+      extendInfo: {
+        LSUIElement: '1'
+      }
+    });
+  });
+})();
